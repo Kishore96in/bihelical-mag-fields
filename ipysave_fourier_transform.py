9603dc3194e205c2da9c48dc9201be5e27bc44b9
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-03-29 13:55:27 +0530

    signed_loglog_plot: return list of handles for legend
    Also allow to specify label, colour etc. for the line

--- ipysave_fourier_transform.py
+++ ipysave_fourier_transform.py
@@ -62,17 +62,21 @@ def calc_spec(fname, K, get_fft=get_data_fft):
 	k = (2*np.pi/L)*np.arange(nk)
 	return k, E, H
 
-def signed_loglog_plot(k, spec, ax):
+def signed_loglog_plot(k, spec, ax, line_params=None):
 	where_pos = np.where(spec>=0)[0]
 	where_neg = np.where(spec<0)[0]
 	spec = np.abs(spec)
 	
 	params_pos = {'facecolors':'none', 'edgecolors':'r', 'marker':'o'}
 	params_neg = {'c':'b', 'marker':'o'}
+	if line_params is None:
+		line_params = dict()
 	
-	ax.loglog(k, spec)
-	ax.scatter(k[where_pos], spec[where_pos], **params_pos, label="_")
-	ax.scatter(k[where_neg], spec[where_neg], **params_neg, label="_")
+	l1 = ax.loglog(k, spec, **line_params)[0]
+	l2 = ax.scatter(k[where_pos], spec[where_pos], **params_pos, label="positive")
+	l3 = ax.scatter(k[where_neg], spec[where_neg], **params_neg, label="negative")
+	
+	return [l1, l2, l3]
 
 if __name__ == "__main__":
 	_, E, H = calc_spec("hmi.b_synoptic_small.2267", K=np.array([0,1]))
