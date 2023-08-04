abb3d43ed9f54e5cdc270c7af076354b7a887f93
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-08-04 13:58:55 +0530

    stats.jackknife: extend comment

--- stats.py
+++ stats.py
@@ -23,6 +23,6 @@ def jackknife(arr, axis):
 	m1 = np.average(means, axis=0)
 	m2 = np.average(means**2, axis=0)
 	var = m2 - m1**2 #biased estimator of variance
-	std = np.sqrt(var*n/(n-1.5)) #less biased estimator of standard deviation, assuming errors are Gaussian. Correction factor copied from Wikipedia (https://en.wikipedia.org/wiki/Standard_deviation#Unbiased_sample_standard_deviation ; accessed 1:55 PM IST, 04-Aug-2023)
+	std = np.sqrt(var*n/(n-1.5)) #less biased estimator of standard deviation, assuming errors are Gaussian and uncorrelated between samples. Correction factor copied from Wikipedia (https://en.wikipedia.org/wiki/Standard_deviation#Unbiased_sample_standard_deviation ; accessed 1:55 PM IST, 04-Aug-2023)
 	
 	return m1, std
