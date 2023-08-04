a4281cccfbac540e3edb7362cfb370f06592c80c
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-08-04 14:17:45 +0530

    reproduce_*: use jackknife error estimates
    Also, the error is calculated only for -np.imag(H1), not H1 itself.

--- reproduce_singh18_HMI.py
+++ reproduce_singh18_HMI.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 from spectrum import calc_spec, signed_loglog_plot
 from read_FITS import get_B_vec
+from stats import jackknife
 
 if __name__ == "__main__":
 	#Figure 2
@@ -28,21 +29,18 @@ if __name__ == "__main__":
 	E0_list = np.array(E0_list)
 	H1_list = np.array(H1_list)
 	
-	E0 = np.average(E0_list, axis=0)
-	E0_err = np.sqrt(np.average((E0_list - E0)**2, axis=0))
-	
-	H1 = np.average(H1_list, axis=0)
-	H1_err = np.sqrt(np.average((H1_list - H1)**2, axis=0))
+	E0, E0_err = jackknife(E0_list, axis=0)
+	nimH1, nimH1_err = jackknife(-np.imag(H1_list), axis=0)
 	
 	fig,axs = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [2,1]})
 	
-	handles = signed_loglog_plot(k, -np.imag(k*H1), axs[0], {'label':"-np.imag(k*H(k,1))"})
+	handles = signed_loglog_plot(k, k*nimH1, axs[0], {'label':"-np.imag(k*H(k,1))"})
 	h = axs[0].loglog(k, E0, label="E(k,0)")
 	handles.extend(h)
 	axs[0].legend(handles=handles)
 	
 	axs[1].loglog(k, E0_err, label="err, E")
-	axs[1].loglog(k, k*H1_err, label="err, kH")
+	axs[1].loglog(k, k*nimH1_err, label="err, kH")
 	axs[1].set_ylabel("Error")
 	axs[1].set_xlabel("k")
 	axs[1].legend()
