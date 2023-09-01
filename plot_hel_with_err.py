b664fdd368d0acbc6310188d48b289dd148e0051
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-09-01 12:51:34 +0530

    plot_hel_with_err: reduce figure size

--- plot_hel_with_err.py
+++ plot_hel_with_err.py
@@ -64,7 +64,7 @@ def plot_hel_with_err(cr_list):
 	
 	cr_list_int = [int(cr) for cr in cr_list]
 	fig.suptitle(f"CR: {min(cr_list_int)}–{max(cr_list_int)}")
-	fig.set_size_inches(6.4,6.4)
+	fig.set_size_inches(4,4)
 	fig.tight_layout()
 	
 	return fig
