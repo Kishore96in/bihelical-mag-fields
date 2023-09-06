73829d1c2415c73a57c69d7bdf05cf2f4eb73ded
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-09-06 17:06:33 +0530

    plot_hel_with_err.plot_hel_with_err: correctly access results

--- plot_hel_with_err.py
+++ plot_hel_with_err.py
@@ -78,13 +78,13 @@ def plot_hel_with_err(cr_list):
 	
 	fig,axs = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [2,1]})
 	
-	handles = signed_loglog_plot(k, k*nimH1, axs[0], {'label':"$-\mathrm{Im}(k\,H(k,K_1))$"})
-	h = axs[0].loglog(k, E0, label="$E(k,0)$")
+	handles = signed_loglog_plot(res.k, res.k*res.nimH1, axs[0], {'label':"$-\mathrm{Im}(k\,H(k,K_1))$"})
+	h = axs[0].loglog(res.k, res.E0, label="$E(k,0)$")
 	handles.extend(h)
 	# axs[0].legend(handles=handles)
 	
-	axs[1].loglog(k, np.abs(nimH1)/nimH1_err, label="$-\mathrm{Im}(k H(k,K_1))$")
-	axs[1].loglog(k, E0/E0_err, label="$E(k,0)$")
+	axs[1].loglog(res.k, np.abs(res.nimH1)/res.nimH1_err, label="$-\mathrm{Im}(k H(k,K_1))$")
+	axs[1].loglog(k, res.E0/res.E0_err, label="$E(k,0)$")
 	axs[1].axhline(1, ls=':', c='k')
 	axs[1].set_ylabel("|data/error|")
 	axs[1].set_xlabel("k")
