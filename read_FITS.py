9ccab9ec8aaa037e7dd7b3b5039bb2d124b06fae
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-09-06 14:18:32 +0530

    read_FITS: experiment with adding some FITS reading classes
    Untested and unused right now.

--- read_FITS.py
+++ read_FITS.py
@@ -50,3 +50,90 @@ def get_B_vec(fname, dbllat=False):
 	B_vec = np.swapaxes(B_vec, -1, -2) #The FITS files would've had spatial coordinates latitude,longitude.
 	
 	return B_vec
+
+class FITSreader():
+	"""
+	Intended to be used like
+	```
+	r = FITSreader(mask_threshold=100, dbllat=True)
+	B_vec = r(fname)
+	```
+	"""
+	def __init__(**kwargs):
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+	
+	def read(self, fname):
+		"""
+		Read FITS files, and return an array [Br, Bp, -Bt] (axis order r,phi,theta). This method does not perform a Fourier transform.
+		"""
+		raise NotImplementedError
+	
+	def stack_latitude(self, data):
+		return data
+	
+	def mask(self, data):
+		"""
+		This allows subclasses to perform any operations on B-vector before the domain is doubled or the FFT is taken.
+		"""
+		return data
+	
+	def fft(self, data):
+		return scipy.fft.fft2(data, norm='forward')
+	
+	def __call__(self, fname):
+		B_vec = self.read(fname)
+		B_vec = self.mask(B_vec)
+		B_vec = self.stack_latitude(B_vec)
+		return self.fft(B_vec)
+
+class StackLatitudeMixin():
+	def stack_latitude(self, data):
+		n_vec, n_lon, n_lat = np.shape(data)
+		if not n_lon == n_lat*2:
+			raise RuntimeError(f"Unexpected FITS data size: {np.shape(data)}")
+		if not n_lat%2 == 0:
+			raise RuntimeError("n_lat is odd, so unclear how to split into two for stacking.")
+		nlatb2 = int(n_lat/2)
+		data = np.concatenate((data[:,:,nlatb2:], data, data[:,:,:nlatb2]), axis=-1)
+		if not np.shape(data) == (n_vec, n_lon, 2*n_lat):
+			raise RuntimeError(f"Something went wrong while stacking: {np.shape(data) = }; expected ({n_vec}, {n_lon}, {2*n_lat})")
+
+class ExciseLatitudeMixin():
+	def mask(self, data):
+		#Assumes the last axis is latitude, and that it extends from -90 to 90.
+		if not hasattr(self, "max_lat"):
+			raise AttributeError("Set max_lat to use this class.")
+		
+		n_lat = np.shape(data)[-1]
+		lat = np.linspace(-90,90,n_lat)
+		return np.where(np.abs(lat) > self.max_lat, 0, data)
+
+class HMIreader(FITSreader):
+	def read(self, fname):
+		Br = get_data_fft(f"{fname}.Br.fits")
+		Bt = get_data_fft(f"{fname}.Bt.fits")
+		Bp = get_data_fft(f"{fname}.Bp.fits")
+		
+		B_vec = np.stack([Br, Bp, -Bt]) #Equation 10 of {SinKapBra18}
+		B_vec = np.swapaxes(B_vec, -1, -2) #The FITS files would've had spatial coordinates latitude,longitude.
+		
+		return B_vec
+
+class HMIreader_dbl(StackLatitudeMixin, HMIreader):
+	pass
+
+class SOLISreader(ExciseLatitudeMixin, FITSreader):
+	def read(self, fname):
+		with fits.open(fname) as f:
+			hdu = f[0]
+			data = hdu.data
+		data = np.nan_to_num(data)
+	
+		B_vec = np.stack([Br, Bp, -Bt]) #Equation 10 of {SinKapBra18}
+		B_vec = np.swapaxes(B_vec, -1, -2) #The FITS files would've had spatial coordinates latitude,longitude.
+		
+		return B_vec
+
+class SOLISreader_dbl(ExciseLatitudeMixin, StackLatitudeMixin, SOLISreader):
+	pass
