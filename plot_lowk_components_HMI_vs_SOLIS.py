bb66eb19ad6245b2bb6be1cd88a001e112cbdfac
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-09-07 13:58:00 +0530

    plot_lowk_components_HMI_vs_SOLIS: better figure formatting

--- plot_lowk_components_HMI_vs_SOLIS.py
+++ plot_lowk_components_HMI_vs_SOLIS.py
@@ -3,6 +3,7 @@ Fourier-filter the HMI and SOLIs synoptic maps for a particular CR and see if th
 """
 
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from read_FITS import HMIreader, SOLISreader
@@ -21,7 +22,12 @@ if __name__ == "__main__":
 	Bvec_h_filt = filter_fourier(Bvec_h, 0, 1e-2, k_axes=[-1,-2], L=[2*np.pi*700, np.pi*700])
 	Bvec_s_filt = filter_fourier(Bvec_s, 0, 1e-2, k_axes=[-1,-2], L=[2*np.pi*700, np.pi*700])
 	
-	fig,axs = plt.subplots(2)
+	fig = plt.figure()
+	gs = mpl.gridspec.GridSpec(1,2, width_ratios=[0.95, 0.05])
+	gsl = mpl.gridspec.GridSpecFromSubplotSpec(2,1, subplot_spec=gs[0], hspace=0.3)
+	ax0 = fig.add_subplot(gsl[0])
+	ax1 = fig.add_subplot(gsl[1])
+	ax_cbar = fig.add_subplot(gs[1])
 	
 	vmin = min(np.min(Bvec_h_filt), np.min(Bvec_s_filt))
 	vmax = max(np.max(Bvec_h_filt), np.max(Bvec_s_filt))
@@ -34,15 +40,19 @@ if __name__ == "__main__":
 		'cmap': 'bwr',
 		}
 	
-	im0 = axs[0].imshow(Bvec_h_filt[0], **im_kwargs)
-	axs[0].set_title("HMI")
-	axs[0].set_ylabel(r"$\lambda$ (degrees)")
+	im0 = ax0.imshow(Bvec_h_filt[0], **im_kwargs)
+	ax0.set_title("HMI")
+	ax0.set_ylabel(r"$\lambda$ (degrees)")
 	
-	im1 = axs[1].imshow(Bvec_s_filt[0], **im_kwargs)
-	axs[1].set_title("SOLIS")
-	axs[1].set_ylabel(r"$\lambda$ (degrees)")
-	axs[1].set_xlabel(r"$\phi$ (degrees)")
+	im1 = ax1.imshow(Bvec_s_filt[0], **im_kwargs)
+	ax1.set_title("SOLIS")
+	ax1.set_ylabel(r"$\lambda$ (degrees)")
+	ax1.set_xlabel(r"$\phi$ (degrees)")
 	
+	c = fig.colorbar(im1, cax=ax_cbar)
+	c.set_label(r"$B_r$")
+	
+	fig.set_size_inches(4,4)
 	fig.tight_layout()
 	
 	plt.show()
